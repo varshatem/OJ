fd3ac26a7d6fd6d68a3f16032c3fe49967b187eb
varsha  tembugade <162706125+varshatem@users.noreply.github.com>
2025-08-30 17:48:05 +0530

    Update Event.js
    Removed commented-out code for the Event model definition.

--- models/Event.js
+++ models/Event.js
@@ -1,35 +1,4 @@
-// const { DataTypes } = require('sequelize');
-// const  sequelize = require("../config/database");
 
-// const Event =  sequelize.define('Event', {
-//     id: {
-//         type : DataTypes.INTEGER,
-//         autoIncrement: true,
-//         primaryKey: true
-//     },
-//     name: {
-//         type: DataTypes.STRING,
-//         allowNull: false,
-//         unique: true
-//     },
-//     start_time: {
-//         type: DataTypes.DATE,
-//         allowNull: true
-//     },
-//     end_time: {
-//         type: DataTypes.DATE,
-//         allowNull: true
-//     },
-//     is_active: {
-//         type: DataTypes.BOOLEAN,
-//         allowNull: false,
-//         defaultValue: false
-//     },
-// }, {
-//     timestamps: true
-// });
-
-// module.exports = Event;
 
 
 const { DataTypes } = require('sequelize');
