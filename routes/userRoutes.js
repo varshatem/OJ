dcbd17b8331a1e86f112f2833d59ef3a1ab0c795
varsha  tembugade <162706125+varshatem@users.noreply.github.com>
2025-09-04 14:37:41 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,9 +3,11 @@ require("dotenv").config();
 const userController = require("../controllers/userController");
 const router = express.Router();
 
-
+const auth = require('../middlewares/authMiddleware')
 
 router.post("/register", userController.registerUser);
 router.post("/login", userController.Login);
 router.post("/register-team", userController.RegisterTeam);
+router.get('/profile', auth, userController.GetProfile);
 module.exports = router;
+
