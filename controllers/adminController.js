759b9e91e51c60976a5a43babaa1d44cf292ec90
varsha  tembugade <162706125+varshatem@users.noreply.github.com>
2025-09-04 14:34:00 +0530

    Update adminController.js

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -73,5 +73,109 @@ exports.loginAdmin = async (req, res) => {
             .status(400)
             .json({ error: "Error logging in User", details: error.message });
     }
+};
+
+exports.getAllUsers = async (req, res) => {
+    try {
+        const users = await User.findAll({
+            attributes: [
+                "id",
+                "username",
+                "email",
+                "is_junior",
+                "role",
+                "created_at",
+                "event_id",
+            ],
+        });
+        res.status(200).json(users);
+    } catch (error) {
+        res
+            .status(500)
+            .json({ error: "Error fetching users", details: error.message });
+    }
 }
+exports.getAllTeams = async (req, res) => {
+    try {
+
+        const teams = await Team.findAll({
+            attributes: [
+                "id",
+                "team_name",
+                "user1_id",
+                "user2_id",
+                "event_id",
+                "is_junior",
+                "score",
+                "correct_submission",
+                "wrong_submission",
+                "submission_time",
+            ],
+            include: [
+                {
+                    model: User,
+                    as: 'Users',
+                    attributes: ['id', 'username', 'email']
+                }
+            ],
+            include: [
+                {
+                    model: Event,
+                    as: 'Event',
+                    attributes: ['id', 'name']
+                }
+            ]
+            
+        });
+        res.status(200).json({ teams });
+    } catch (error) {
+        res.status(500).json({ error: "Error fetching teams", details: error.message });
+    }
+}
+exports.getAllProblems = async (req, res) => {
+    try {
+        const problems = await Problem.findAll({
+            attributes: [
+                "id",
+                "title",
+                "is_junior",
+                "event_id",
+                "created_at",
+                "test_case_path",
+            ],
+        });
+        res.status(200).json({ problems });
+    } catch (error) {
+        res.status(500).json({ error: "Error fetching problems", details: error.message });
+    }
+}
+
+
+exports.getAllSubmissions = async (req, res) => {
+    try {
+        const submissions = await Submission.findAll({
+            include: [
+                {
+                    model: Problem,
+                    as: 'Problem',
+                    attributes: ['title']
+                },
+                {
+                    model: Team,
+                    as: 'Team',
+                    attributes: ['team_name']
+                }
+            ],
+            attributes: ['id', 'team_id', 'problem_id', 'result',  'submitted_at', 'code','language']
+        });
+        res.status(200).json({ submissions });
+    } catch (error) {
+        res.status(500).json({ error: "Error fetching submissions", details: error.message });
+    }
+}
+
+
+
+
+
 
