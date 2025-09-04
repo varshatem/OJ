bbc99f4f31ea857ece21942c7aef4dc300b07686
varsha  tembugade <162706125+varshatem@users.noreply.github.com>
2025-09-04 14:37:09 +0530

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,13 +1,16 @@
 const express = require("express");
-const adminAuthenticate = require("../middlewares/adminMiddleware");
-const multer = require("multer"); // Make sure this is correctly imported
-const fs = require("fs");
-const path = require("path");
+const adminAuth= require("../middlewares/adminMiddleware");
 const router = express.Router();
 
 const adminController = require("../controllers/adminController");
 
 router.post("/register", adminController.registerAdmin);
 router.post("/login", adminController.loginAdmin);
+router.get("/problems", adminAuth, adminController.getAllProblems);
+router.get("/users", adminAuth, adminController.getAllUsers);
 
+router.get("/teams", adminAuth, adminController.getAllTeams);
+
+router.get("/problems", adminAuth, adminController.getAllProblems);
 module.exports = router;
+
