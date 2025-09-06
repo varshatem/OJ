52f245e36b02dc90eaf320bfe5462de2073e01eb
varsha  tembugade <162706125+varshatem@users.noreply.github.com>
2025-09-06 13:00:15 +0530

    Update problemroutes.js

--- routes/problemroutes.js
+++ routes/problemroutes.js
@@ -2,24 +2,25 @@
 const express = require("express");
 const router = express.Router();
 const problemcontroller = require("../controllers/problemcontroller");
-const authMiddleware = require("../middlewares/authMiddleware")
+const adminAuthenticate= require("../middlewares/authMiddleware");
+const authenticateToken = require("../middlewares/authMiddleware");
 // Create a new problem
-router.post("/create",authMiddleware, problemcontroller.createProblem);
+router.post("/create",adminAuthenticate, problemcontroller.createProblem);
 
 // Get all problems
-router.get("/", problemcontroller.getAllProblems);
+router.get("/", authenticateToken,problemcontroller.getAllProblems);
 
 // Get a single problem by ID
-router.get("/:id", problemcontroller.getProblemById);
+router.get("/:id", authenticateToken,problemcontroller.getProblemById);
 
 // Update a problem
-router.put("/:id", authMiddleware, problemcontroller.updateProblem);
+router.put("/:id", adminAuthenticate, problemcontroller.updateProblem);
 
 // Add a new sample to a problem
-router.post("/:id/samples", authMiddleware, problemcontroller.addSample);
+router.post("/:id/samples", adminAuthenticate, problemcontroller.addSample);
 
 // Delete a problem
-router.delete("/:id", authMiddleware, problemcontroller.deleteProblem);
+router.delete("/:id", adminAuthenticate, problemcontroller.deleteProblem);
 
-router.put("/:id/samples", authMiddleware, problemcontroller.updateSample);
+router.put("/:id/samples", adminAuthenticate, problemcontroller.updateSample);
 module.exports = router;
