9a220bc9c46b63bd6d70b981f97ab83fbf2e766d
varsha  tembugade <162706125+varshatem@users.noreply.github.com>
2025-09-06 12:49:16 +0530

    Implement missing fields validation in controllers

--- controllers/poblemcontroller.js
+++ controllers/poblemcontroller.js
@@ -8,6 +8,14 @@ exports.createProblem = async (req, res) => {
 
     // create problem first
     const problem = await Problem.create({  title, description,score,input_format,output_format,consraints,is_junior,time_limit,memory_limit,event_id, });
+    const missingFields = problem.filter(field => !(field in req.body));
+
+    if (missingFields.length > 0) {
+      return res.status(400).json({
+        error: "Missing required fields",
+        missing: missingFields
+      });
+    }
 
     // if samples are provided, bulk insert them
     if (samples && Array.isArray(samples)) {
@@ -62,6 +70,15 @@ exports.addSample = async (req, res) => {
   try {
     const { id } = req.params; // problem ID
     const { input, output, explanation } = req.body;
+    const data = req.body();
+const missingFields = data.filter(field => !(field in req.body));
+
+    if (missingFields.length > 0) {
+      return res.status(400).json({
+        error: "Missing required fields",
+        missing: missingFields
+      });
+    }
 
     const problem = await Problem.findByPk(id);
     if (!problem) return res.status(404).json({ error: "Problem not found" });
@@ -161,3 +178,43 @@ exports.updateProblem = async (req, res) => {
   }
 };
 
+exports.updateSample = async (req, res) => {
+  try {
+    const { id } = req.params; // sample ID from URL
+    const { input, output, explanation } = req.body;
+
+    const sample = await ProblemSample.findByPk(id);
+    if (!sample) {
+      return res.status(404).json({ error: `Sample with ID ${id} not found` });
+    }
+
+    // partial update (only update provided fields)
+    sample.input = input || sample.input;
+    sample.output = output || sample.output;
+    sample.explanation = (explanation !== undefined) ? explanation : sample.explanation;
+
+    await sample.save();
+
+    return res.status(200).json({ message: "Sample updated successfully", sample });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Failed to update sample" });
+  }
+};
+// Delete a sample test case by sample ID
+exports.deleteSample = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deleted = await ProblemSample.destroy({ where: {id} });
+
+    if (!deleted) {
+      return res.status(404).json({ error: "Sample not found" });
+    }
+
+    return res.json({ message: "Sample deleted successfully" });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Failed to delete sample" });
+  }
+};
