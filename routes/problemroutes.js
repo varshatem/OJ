d7a764a388a680826a2085249eaf68e880a0dcc8
varsha  tembugade <162706125+varshatem@users.noreply.github.com>
2025-09-06 12:21:33 +0530

    Update problemroutes.js

--- routes/problemroutes.js
+++ routes/problemroutes.js
@@ -2,9 +2,9 @@
 const express = require("express");
 const router = express.Router();
 const problemcontroller = require("../controllers/problemcontroller");
-
+const authMiddleware = require("../middlewares/authMiddleware")
 // Create a new problem
-router.post("/create", problemcontroller.createProblem);
+router.post("/create",authMiddleware, problemcontroller.createProblem);
 
 // Get all problems
 router.get("/", problemcontroller.getAllProblems);
@@ -13,12 +13,13 @@ router.get("/", problemcontroller.getAllProblems);
 router.get("/:id", problemcontroller.getProblemById);
 
 // Update a problem
-router.put("/:id", problemcontroller.updateProblem);
+router.put("/:id", authMiddleware, problemcontroller.updateProblem);
 
 // Add a new sample to a problem
-router.post("/:id/samples", problemcontroller.addSample);
+router.post("/:id/samples", authMiddleware, problemcontroller.addSample);
 
 // Delete a problem
-router.delete("/:id", problemcontroller.deleteProblem);
+router.delete("/:id", authMiddleware, problemcontroller.deleteProblem);
 
+router.put("/:id/samples", authMiddleware, problemcontroller.updateSample);
 module.exports = router;
