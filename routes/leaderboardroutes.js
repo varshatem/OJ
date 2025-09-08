26393f44f7639be4ed4273d34c7928747a5e665d
Varsha Tembugade <varshatembugade346@gmail.com>
2025-09-08 14:28:05 +0530

    push to main

--- routes/leaderboardroutes.js
+++ routes/leaderboardroutes.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const leaderboardController = require("../controllers/leaderboardController");
 
+const authenticateToken = require("../middlewares/authMiddleware");
 // Update leaderboard after a submission (called by judge after verdict)
 router.post("/update", leaderboardController.updateLeaderboard);
 
 // Get leaderboard for an event
-router.get("/:event_id", leaderboardController.getLeaderboard);
+router.get("/:event_id", authenticateToken, leaderboardController.getLeaderboard);
 
 module.exports = router;
 
