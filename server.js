48b95e4fadcd23367217e456d8ebbd7b9fd27823
Arjun <arjunsgawande123321@gmail.com>
2025-09-09 18:39:09 +0530

    server update

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+require("dotenv").config();
 const http = require("http");
 const cors = require("cors");
+const sequelize = require("./config/database");
+const { syncDB } = require('./models');
 const cookieParser = require('cookie-parser');
 const { initSocket } = require('./SocketConnection');
 const adminRoutes = require('./routes/adminRoutes');
@@ -11,8 +14,15 @@ const leaderboardRoutes = require('./routes/leaderboardroutes');
 const submissionRoutes = require('./routes/submissionRoutes');
 const webhookRoutes = require('./routes/WebhookRoutes');
 const app = express();
-app.use(express.json());
-app.use(cookieParser());
+app.use(express.json());  // To handle JSON payloads
+app.use(cookieParser())
+//const PORT = process.env.PORT || 5000;
+const cors = require("cors");
+
+// CORS configuration
+const allowedOrigins = process.env.ALLOWED_ORIGINS 
+? process.env.ALLOWED_ORIGINS.split(',') 
+: ["http://localhost:5173", "http://localhost:3000"];
 
 // Allow all origins
 app.use(cors({
@@ -22,7 +32,9 @@ app.use(cors({
 }));
 
 const server = http.createServer(app);
-
+(async () => {
+    await syncDB();
+})();
 // Routes
 app.get("/", (req, res) => res.send("🚀 Online Judge API is running..."));
 app.use("/admin", adminRoutes);
@@ -41,6 +53,21 @@ initSocket(server,{
     }
 });
 
-server.listen(PORT, () => {
-    console.log(`🚀 Server running at http://localhost:${PORT}`);
-});
+sequelize.sync()
+  .then(() => {
+    console.log("✅ Database connected & synced");
+
+    initSocket(server, {
+      cors: {
+        origin: "*",
+        credentials: true,
+      },
+    });
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ Failed to connect DB:", err);
+  });
